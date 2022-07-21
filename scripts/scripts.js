b23c88d87eec005967ad9daf0993296083efce17
Carlos Acero <carlos.acero@innovacer.mx>
2022-07-20 21:26:31 -0500

    Adding topnav mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,3 +15,12 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function topnavFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
